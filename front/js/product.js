d813d63d5e3641ee84bdd5de10e7ad6f399ce143
changriha neijma <changriha.neijma@orange.fr>
2021-10-27 09:52:04 +0200

    panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,10 @@
 const params = new URLSearchParams(window.location.search);
 const productID = params.get("id");
+const cart = [];
+let product;
 
-getProduct(productID).then((product) => {
-  console.log(product);
+getProduct(productID).then((productData) => {
+  product = productData;
   const title = document.getElementById("title");
   title.innerHTML = product.name;
   const price = document.getElementById("price");
@@ -15,10 +17,6 @@ getProduct(productID).then((product) => {
 
   const choose = document.getElementById("colors");
 
-  let option = document.createElement("option");
-  option.innerHTML = "SVP, choisissez une couleur";
-  choose.appendChild(option);
-
   for (const color of product.colors) {
     let numberColor = document.createElement("option");
     numberColor.value = `html/product.html?id=${color}
@@ -26,4 +24,20 @@ getProduct(productID).then((product) => {
     numberColor.innerHTML = color;
     choose.appendChild(numberColor);
   }
+});
+const btnCart = document.getElementById("addToCart");
+btnCart.addEventListener("click", () => {
+  // si la quantité est a 0 ne rien faire . si la quantité est supérieur a 100 ne rien faire et ajouté un message d'erreur.
+  // verifier si le produit est deja dans le panier . si c'est le cas ne pas poussé le produit mais juste au
+  // augmenter la quantité. sinon poussé le produit .
+  const quantity = document.getElementById("quantity").value;
+
+  cart.push({
+    id: product._id,
+    name: product.name,
+    quantity: quantity,
+  });
+  console.log(cart);
+
+  alert("Votre article a bien été ajouté au panier !");
 });
